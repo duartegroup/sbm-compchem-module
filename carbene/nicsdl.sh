b3c8e22766839a15d0c52858aace837d396c74d7
Jonathan Yong <yongrenjie@gmail.com>
2020-01-08 16:31:10 +0000

    Change target from yongrenjie to duartegroup

--- carbene/nicsdl.sh
+++ carbene/nicsdl.sh
@@ -1,6 +1,6 @@
 dl_nics () {
-    wget https://raw.githubusercontent.com/yongrenjie/sbmcc/master/carbene/nics1.py -O ~/nics1.py
-    wget https://raw.githubusercontent.com/yongrenjie/sbmcc/master/carbene/nics2.py -O ~/nics2.py
+    wget https://raw.githubusercontent.com/duartegroup/sbmcc/master/carbene/nics1.py -O ~/nics1.py
+    wget https://raw.githubusercontent.com/duartegroup/sbmcc/master/carbene/nics2.py -O ~/nics2.py
     if [[ ":$PATH" != *":$HOME:"* ]]; then
         echo 'export PATH=$HOME:$PATH' >> ~/.bash_profile
         source ~/.bash_profile
