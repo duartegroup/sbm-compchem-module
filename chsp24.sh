8d1083f51ba77a3c993c9710bd7928f3415c2ea7
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2025-10-13 14:40:34 +0100

    Scripts for ARC (CSHP) (#10)
    * Add qorca for ARC
    
    * add shell script

---
+++ chsp24.sh
@@ -0,0 +1,25 @@
+dl_qorca () {
+    mkdir -p ~/bin/
+    wget https://raw.githubusercontent.com/duartegroup/sbm-compchem-module/master/qorcaARC.py -O ~/bin/qorca
+    chmod +x ~/bin/qorca
+}
+
+dl_qpython_autode () {
+    wget https://raw.githubusercontent.com/duartegroup/sbm-compchem-module/master/qpythonAutodE.py -O ~/bin/qpython
+    chmod +x ~/bin/qpython
+    if ! command -v conda &> /dev/null; then
+        print "Installing conda..."
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -q -O miniconda.sh
+        # The installer prints loads to the terminal, so redirect it (so it doesn't appear)
+        eval "bash miniconda.sh -b -p \"$HOME/miniconda\"" > /dev/null
+        rm miniconda.sh
+        eval "$("$HOME"/miniconda/bin/conda shell.bash hook)" > /dev/null
+        eval "conda init bash" > /dev/null
+        print "done\n"
+        print "Installed miniconda to $HOME/miniconda with\n"
+        python --version
+    fi
+}
+
+dl_qorca
+dl_qpython_autode
