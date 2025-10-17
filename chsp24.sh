21328646e35eea6bfa13bcffa541d5091e6d2f81
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2025-10-17 11:27:50 +0100

    Bugfix scripts for ARC
    * Bugfix scripts
    
    * add qpython
    
    * update qpython

--- chsp24.sh
+++ chsp24.sh
@@ -5,18 +5,18 @@ dl_qorca () {
 }
 
 dl_qpython_autode () {
-    wget https://raw.githubusercontent.com/duartegroup/sbm-compchem-module/master/qpythonAutodE.py -O ~/bin/qpython
+    wget https://raw.githubusercontent.com/duartegroup/sbm-compchem-module/master/qpythonARC.py -O ~/bin/qpython
     chmod +x ~/bin/qpython
     if ! command -v conda &> /dev/null; then
-        print "Installing conda..."
+        echo "Installing conda..."
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -q -O miniconda.sh
         # The installer prints loads to the terminal, so redirect it (so it doesn't appear)
         eval "bash miniconda.sh -b -p \"$HOME/miniconda\"" > /dev/null
         rm miniconda.sh
         eval "$("$HOME"/miniconda/bin/conda shell.bash hook)" > /dev/null
         eval "conda init bash" > /dev/null
-        print "done\n"
-        print "Installed miniconda to $HOME/miniconda with\n"
+        echo "done\n"
+        echo "Installed miniconda to $HOME/miniconda with\n"
         python --version
     fi
 }
