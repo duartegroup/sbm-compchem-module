669f2efb933bf119999f2fe4e24353e6a681d5bf
Shoubhik Maiti <17470159+shoubhikraj@users.noreply.github.com>
2025-10-19 15:37:04 +0100

    Change default time argument from '1-12:00:00' to '2-00:00:00'

--- qpythonARC.py
+++ qpythonARC.py
@@ -43,7 +43,7 @@ def get_args():
     
     parser.add_argument("-t", "--time",
                         action='store',
-                        default="1-12:00:00",
+                        default="2-00:00:00",
                         help='Specify the runtime for the job, in format [d-]hh:mm:ss')
 
     parser.add_argument("-args", '--extra_arguments',
